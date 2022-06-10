0ba5649519676317a3b680bf7aa85f2a98cea82d
Sergey <maletssergey@gmail.com>
2022-06-10 17:15:57 +0300

    start_files

--- hello_django/hello_django/views.py
+++ hello_django/hello_django/views.py
@@ -5,4 +5,9 @@ def about(request):
     return render(request, "about.html", {"greteeng":'hello serg',"mama":"Ira","papa":"vova"})
 
 def home(requst):
-    return HttpResponse('This is my home')
+    return render(requst, "home.html")
+
+def reverse(requst):
+    user_text = requst.GET['usertext']
+    reverse = user_text[::-1]
+    return render(requst, "reverse.html",{'key':reverse})
